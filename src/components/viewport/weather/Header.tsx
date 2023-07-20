891ca7491dab69e57a80342e853bef2c87efb372
Rebecca <email@rebecca.sh>
2023-07-20 16:02:25 +1000

    Fix bug I accidentally ctrl-z'd

--- src/components/viewport/weather/Header.tsx
+++ src/components/viewport/weather/Header.tsx
@@ -4,7 +4,7 @@ const Header = ({props, loading}: {props: IWeather, loading: boolean}) => {
     var loadingDefault = loading ? 'bg-white bg-opacity-10 rounded-lg' : 'bg-transparent';
     return(
         <div className="w-full grid place-items-center mb-10">
-            <div className={`grid place-items-center text-white gap-4 drop-shadow-lg ${loading ? 'text-transparent' : 'text-white'}`}>
+            <div className={`grid place-items-center gap-4 drop-shadow-lg ${loading ? 'text-transparent' : 'text-white'}`}>
                 <h2 className={`text-5xl ${loadingDefault}`}>{props.resolvedAddress}</h2>
                 <h3 className={`text-8xl ${loadingDefault}`}>{Math.round(props.currentConditions.temp)}<span className="absolute">°</span></h3>
                 <h3 className={`text-4xl ${loadingDefault}`}>{props.currentConditions.conditions}</h3>
